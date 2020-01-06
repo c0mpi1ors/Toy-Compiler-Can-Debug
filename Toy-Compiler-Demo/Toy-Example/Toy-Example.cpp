4a8218822836ab8e8b1264191a734d5fcc8d9a37
evanzhou2000 <evanzhou2000@gmail.com>
2020-01-06 12:53:21 -0500

    create project

---
+++ Toy-Compiler-Demo/Toy-Example/Toy-Example.cpp
@@ -0,0 +1,12 @@
+// Toy-Example.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+extern "C" int show();
+
+int main()
+{
+    show();
+}
+
---
+++ Toy-Compiler-Demo/Toy-Example/native.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+extern "C"
+void printi(long long val)
+{
+    printf("%lld\n", val);
+}
+
+
